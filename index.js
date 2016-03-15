2b0cd8766e98b1ca83cbdb51b0fb67bf48d275ce
Gabor Csardi <csardi.gabor@gmail.com>
2016-03-15 15:19:34 +0000

    First implementation

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 
-function parse(stream) {
+var fs = require('fs');
+var byline = require('byline');
 
+function parse(file) {
+    var stream = byline(fs.createReadStream(file, { encoding: 'utf8' }));
+    var desc = { };
+    var current = '';
+
+    stream.on('readable', function() {
+	var line;
+	while (null !== (line = stream.read())) {
+	    if (line.match(/^[^\s]/) && current !== '') {
+		var rec = split_record(current);
+		desc[ rec.key ] = rec.value;
+		current = line;
+	    } else {
+		current = current + '\n' + line;
+	    }
+	}
+    });
+
+    return desc;
+}
+
+function split_record(str) {
+    var colon = str.indexOf(":");
+    if (colon < 0) { throw ("Invalid record: " + str); return; }
+    return { 'key': str.substr(0, colon).trim(),
+	     'value': str.substr(colon + 1).trim() };
 }
 
 module.exports = parse;
